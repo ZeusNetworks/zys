70ae13aaf17f7406cffed5bef29af227e2af4057
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-19 21:46:19 +0700

    Make it perfect

--- setup.py
+++ setup.py
@@ -1,35 +1,31 @@
-from setuptools import setup
+import setuptools
 
+with open('requirements.txt') as f:
+    dependencies = [l.strip() for l in f]
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-setup(
-    name="zyspy",
-    version="0.4",
-    description="zeus API wrapper",
-    py_modules=["zyspy"],
-    package_dir={'': 'zeus'},
-    install_requires=["requests", "typing"],
-    extras_require={
-                      "dev":[
-                          "pytest",
-                      ],},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.9.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.10.1",
-        "Operating System :: OS Independent",
-    ],
+setuptools.setup(
+    name='zeus',
+    version='0.1',
+    description='Zeus Wrapper',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ZeusNetworks/Zys-Py",
-    author="ZeusNetworks"
-
+    license='License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+    author='ZeusNetworks',
+    author_email='-',
+    url='https://github.com/ZeusNetworks/Syl-Py.git',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Typing :: Typed'
+    ],
+    install_requires=dependencies,
+    python_requires='>=3.9'
 )
